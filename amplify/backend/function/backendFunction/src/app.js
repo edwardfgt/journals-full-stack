564829f0ff50a24ca931ceacd3e3ef27e86a351b
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-06-14 23:48:22 +0100

    Initialise REST api with Amplify

---
+++ amplify/backend/function/backendFunction/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const awsServerlessExpressMiddleware = require("aws-serverless-express/middleware");
+
+// declare a new express app
+const app = express();
+app.use(bodyParser.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
+
+// Enable CORS for all methods
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+app.get("/api", function (req, res) {
+  res.json({ success: "get call succeed!", url: req.url });
+});
+
+module.exports = app;

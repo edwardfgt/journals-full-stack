d55ddc42fd1f3ef9f50f444b72ab24b8de9c9886
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-06-17 22:02:00 +0100

    Setup react-router-dom and homepage route as a test

--- src/App.js
+++ src/App.js
@@ -1,44 +1,33 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Amplify, { API } from "aws-amplify";
 import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/home/home";
 
 function App() {
-  useEffect(() => {
-    testAPI();
-  }, []);
+  // const [myMessage, setMyMessage] = useState("");
 
-  const [myMessage, setMyMessage] = useState("");
-
-  async function testAPI() {
-    API.get("backend", "/api", {})
-      .then((response) => {
-        setMyMessage(response.success);
-        console.log(`response: ${response}`);
-      })
-      .catch((error) => {
-        console.log(error.response);
-      });
-  }
+  // async function testAPI() {
+  //   API.get("backend", "/api", {})
+  //     .then((response) => {
+  //       setMyMessage(response.success);
+  //       console.log(`response: ${response}`);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error.response);
+  //     });
+  // }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>{myMessage}</p>
-      </header>
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 

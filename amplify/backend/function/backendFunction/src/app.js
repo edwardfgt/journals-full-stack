570224ce08feecf45192148386caf3446f086aa9
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-06-18 22:37:03 +0100

    Create new email route using aws simple email service

--- amplify/backend/function/backendFunction/src/app.js
+++ amplify/backend/function/backendFunction/src/app.js
@@ -1,3 +1,5 @@
+import aws from "aws-sdk";
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const awsServerlessExpressMiddleware = require("aws-serverless-express/middleware");
@@ -17,19 +19,56 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get("/api", async function (req, res) {
+app.post("/email", async function (req, res) {
   try {
     const secretData = await secretsManager
       .getSecretValue({ SecretId: "journals/aws" })
       .promise();
 
     const secretValues = JSON.parse(secretData.SecretString);
-    console.log(`${secretValues.API_KEY}`);
-    res.json({ success: `${secretValues.API_KEY}`, url: req.url });
-  } catch (error) {
-    console.error("Error getting secret", error);
-    res.status(500).json({ error: "Error getting secret" });
+
+    const ses = new aws.SES({
+      accessKeyId: secretValues.API_KEY,
+      secretAccessKey: secretValues.API_SECRET,
+      region: "us-east-1",
+    });
+
+    const { email, message, subject } = req.body;
+
+    sesTest(ses, "mail@journals.gg", "ed@journals.gg", email, message, subject)
+      .then((val) => {
+        console.log("returned" + val);
+        res.send("successful");
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("An error occurred: " + err);
+      });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("An error occurred: " + err);
   }
 });
+
+function sesTest(ses, emailTo, emailFrom, email, message, subject) {
+  console.log("Input parameters:", emailTo, emailFrom, message, subject);
+  const params = {
+    Destination: {
+      ToAddresses: [emailTo],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "Sender: " + email + "\n" + "Message " + message,
+        },
+      },
+      Subject: {
+        Data: `CONTACT FORM: ${subject}`,
+      },
+    },
+    Source: emailFrom,
+  };
+  return ses.sendEmail(params).promise();
+}
 
 module.exports = app;

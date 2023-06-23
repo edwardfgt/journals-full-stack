adaf02c7a62720f23a8e6253afb56f5cde68271d
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-06-23 17:22:54 +0100

    Create navbar component and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home/home";
-import Contact from "./components/home/contact/contact";
+import Contact from "./components/contact/contact";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar currentPath={window.location.pathname} />
       <div className="flex flex-col min-h-screen">
         <div className="flex-1">
           <Routes>

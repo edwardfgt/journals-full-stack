5c83e66c94465b6e21293ae8d12d62aa7457df58
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-06-26 10:51:19 +0100

    Create newsletter grid component

---
+++ src/components/newsletter-grid/newsletterGrid.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import NewsletterModal from "../newsletter-modal/newsletter-modal";
+
+const NewsletterGrid = ({ newsletters }) => {
+  const [open, setOpen] = useState(false);
+  const [currentModal, setCurrentModal] = useState(null);
+
+  const handleOpen = (newsletter) => {
+    setCurrentModal(newsletter);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setCurrentModal(null);
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <div className="flex justify-center mb-40">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-3 gap-6">
+          {newsletters.map((newsletter, index) => (
+            <div
+              className="card w-96 bg-base-100 shadow-xl image-full hover:border"
+              key={index}
+              onClick={() => handleOpen(newsletter)}
+            >
+              <figure>
+                <img src={newsletter.image} alt={newsletter.name} />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{newsletter.name}</h2>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <NewsletterModal
+        open={open}
+        onClose={handleClose}
+        newsletter={currentModal}
+      />
+    </>
+  );
+};
+
+export default NewsletterGrid;

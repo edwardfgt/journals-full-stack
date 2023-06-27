1af97eb483d5c2738f157922ab50b4f135f0ca03
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-06-27 14:30:12 +0100

    Update middleware function with specific headers

--- amplify/backend/function/backendFunction/src/app.js
+++ amplify/backend/function/backendFunction/src/app.js
@@ -14,7 +14,10 @@ app.use(awsServerlessExpressMiddleware.eventContext());
 // Enable CORS for all methods
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     return res.status(200).json({});

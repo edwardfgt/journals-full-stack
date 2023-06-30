7248831f3431785cb71243d4392e0c16a11dcfe9
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-06-30 13:49:48 +0100

    Update pub IDs for other publidations

--- src/assets/newsletters.js
+++ src/assets/newsletters.js
@@ -7,7 +7,7 @@ const newsletters = [
     name: "Investing Journal",
     description:
       "Bitesize market-moving news, summaries and links from the world of investing, three times a week.",
-    pubID: "pub_68e225b7-c08a-4e63-aa31-3e50a50d2083",
+    pubID: "pub_30a85634-38e9-43fc-b0f1-2b3d79f1228c",
     image: wallstImg,
     href: "https://www.investingjournal.gg/",
   },
@@ -15,7 +15,7 @@ const newsletters = [
     name: "Crypto Journal",
     description:
       "Become a better crypto investor in just 5 minutes. Bitesize market-moving news, summaries and links from the world of crypto and web3.",
-    pubID: "pub_68e225b7-c08a-4e63-aa31-3e50a50d2083",
+    pubID: "pub_e3126c39-9397-494e-a4ad-395e27c857ad",
     image: cryptoImg,
     href: "https://crypto.journals.gg/",
   },

a7f9623eda5d92329ac49e4f84891dacca2f6817
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-09-20 20:43:14 +0100

    Update styling for RecentPosts component

--- src/components/recent-posts/recent-posts.js
+++ src/components/recent-posts/recent-posts.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 
 const RecentPosts = ({ url }) => {
-  const [xmlData, setXmlData] = useState(null);
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -23,7 +22,6 @@ const RecentPosts = ({ url }) => {
       .catch((error) => {
         console.error("Error fetching XML data:", error);
       });
-    console.log(xmlData);
   }, []);
 
   console.log(posts);
@@ -41,33 +39,35 @@ const RecentPosts = ({ url }) => {
               {posts.map((post, index) => (
                 <article className="flex flex-col items-start justify-between">
                   <div className="relative w-full">
-                    <img
-                      src={post.image}
-                      alt=""
-                      className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
-                    />
+                    <a href={post.link}>
+                      <img
+                        src={post.image}
+                        alt="Investing Journal edition preview"
+                        className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+                      />
+                    </a>
                     <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
                   </div>
                   <div className="max-w-xl">
                     <div className="mt-8 flex items-center gap-x-4 text-xs">
-                      <time dateTime={post.pubDate} className="text-gray-500">
+                      <time dateTime={post.pubDate} className="text-gray-300">
                         {post.pubDate}
                       </time>
                     </div>
                     <div className="group relative">
-                      <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                      <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-300 group-hover:text-gray-600">
                         <a href={post.link}>
                           <span className="absolute inset-0" />
                           {post.title}
                         </a>
                       </h3>
-                      <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                      <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-300">
                         {post.description}
                       </p>
                     </div>
                     <div className="relative mt-8 flex items-center gap-x-4">
                       <div className="text-sm leading-6">
-                        <p className="font-semibold text-gray-900"></p>
+                        <p className="font-semibold text-gray-300"></p>
                       </div>
                     </div>
                   </div>

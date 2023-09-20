0587265cc571f6312a7e73f84a1f671b9f4b30f2
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2023-09-20 20:35:03 +0100

    Update Recent Posts to take a URL as a prop and edit some styling

--- src/components/recent-posts/recent-posts.js
+++ src/components/recent-posts/recent-posts.js
@@ -1,11 +1,10 @@
 import { useState, useEffect } from "react";
 
-const RecentPosts = () => {
+const RecentPosts = ({ url }) => {
   const [xmlData, setXmlData] = useState(null);
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    const url = `https://jtz9p1w4ne.execute-api.us-east-1.amazonaws.com/dev/rss`;
     const options = {
       method: "GET",
       headers: {
@@ -34,12 +33,9 @@ const RecentPosts = () => {
         <div>
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
-              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                From the blog
+              <h2 className="text-3xl font-bold tracking-tight text-gray-300 sm:text-4xl">
+                Investing Journal
               </h2>
-              <p className="mt-2 text-lg leading-8 text-gray-600">
-                Learn how to grow your business with our expert advice.
-              </p>
             </div>
             <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
               {posts.map((post, index) => (
